694defb9c3009f499ebf3659c44e00e81ffd291d
Rafael Ribeiro <128828470+rafaribeiro2013@users.noreply.github.com>
2026-04-12 20:16:10 +0000

    pagina portal - versao inicial

--- projeto-letterboxd/NossoProjeto/main/models.py
+++ projeto-letterboxd/NossoProjeto/main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from account.models import Pessoa
+#from account.models import Pessoa
 
 # Create your models here.
 class Pessoa(models.Model):
