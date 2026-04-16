32c798b6fc674dd31470f822366b6dd8380cd341
Rafael Ribeiro <128828470+rafaribeiro2013@users.noreply.github.com>
2026-04-16 11:42:07 +0000

    implementação parcial do user personalizado

--- projeto-letterboxd/NossoProjeto/accounts/models.py
+++ projeto-letterboxd/NossoProjeto/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    data_nascimento = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+    foto_perfil = models.ImageField(upload_to='static/accounts/img/perfil_fotos/', default='static/accounts/img/padrao.png') #quero puxar a foto padrao.png do static/fotos 
+
+    def __str__(self):
+        return self.username
