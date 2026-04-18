3032d5189a1135885568a7548361ae23bb86163f
Luisa Ferreira da Silveira <112108551+LuisaSilveira@users.noreply.github.com>
2026-04-18 23:01:50 +0000

    melhoria de css

--- projeto-letterboxd/NossoProjeto/main/views.py
+++ projeto-letterboxd/NossoProjeto/main/views.py
@@ -41,38 +41,32 @@ class MidiaListView(View):
 
 
 class PessoaCreateView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         contexto = {
             'formulario': PessoaModel2Form(),
-            'titulo': 'Cadastrar pessoa',
-            'botao': 'Salvar pessoa',
         }
         return render(request, 'main/formulario.html', contexto)
 
     def post(self, request, *args, **kwargs):
-        formulario = PessoaModel2Form(request.POST, request.FILES)
+        formulario = PessoaModel2Form(request.POST)
         if formulario.is_valid():
             formulario.save()
             return HttpResponseRedirect(reverse_lazy('portal:home'))
 
         contexto = {
             'formulario': formulario,
-            'titulo': 'Cadastrar pessoa',
-            'botao': 'Salvar pessoa',
         }
         return render(request, 'main/formulario.html', contexto)
 
 
 class AvaliacaoCreateView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         contexto = {
             'formulario': AvaliacaoModel2Form(),
-            'titulo': 'Cadastrar avaliação',
-            'botao': 'Salvar avaliação',
         }
-        return render(request, 'main/formulario.html', contexto)
+        return render(request, 'main/criaAvaliacao.html', contexto)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         formulario = AvaliacaoModel2Form(request.POST)
         if formulario.is_valid():
             formulario.save()
@@ -80,10 +74,8 @@ class AvaliacaoCreateView(View):
 
         contexto = {
             'formulario': formulario,
-            'titulo': 'Cadastrar avaliação',
-            'botao': 'Salvar avaliação',
         }
-        return render(request, 'main/formulario.html', contexto)
+        return render(request, 'main/criaAvaliacao.html', contexto)
 
 class AvaliacaoUpdateView(View):
     def get(self, request, pk, *args, **kwargs):
