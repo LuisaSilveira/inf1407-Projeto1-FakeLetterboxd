dcbadcc4345c0c2307145bd7a5f7da91a2adcd5c
Luisa Ferreira da Silveira <112108551+LuisaSilveira@users.noreply.github.com>
2026-04-20 01:53:41 +0000

    consertando conflito de modelo de usuario

--- projeto-letterboxd/NossoProjeto/NossoProjeto/settings.py
+++ projeto-letterboxd/NossoProjeto/NossoProjeto/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Usa o modelo de usuário customizado da app accounts.
+AUTH_USER_MODEL = "accounts.CustomUser"
