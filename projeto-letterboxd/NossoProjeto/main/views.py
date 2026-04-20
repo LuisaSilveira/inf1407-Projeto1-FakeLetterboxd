9449eadced6adc2be0fca0aa139c9226290f8a77
Luisa Ferreira da Silveira <112108551+LuisaSilveira@users.noreply.github.com>
2026-04-20 02:41:56 +0000

    primeira parte da busca de midia com a api omdb feita

--- projeto-letterboxd/NossoProjeto/main/views.py
+++ projeto-letterboxd/NossoProjeto/main/views.py
@@ -8,6 +8,9 @@ from main.models import Midia, Avaliacao, Pessoa
 
 from django.shortcuts import render, get_object_or_404
 
+from main.services.omdb_service import OMDBService
+from django.contrib import messages
+
 # Create your views here.
 
 class AvaliacaoListView(View):
@@ -104,3 +107,54 @@ class AvaliacaoDeleteView(View):
         avaliacao = Avaliacao.objects.get(pk=pk)
         avaliacao.delete()
         return HttpResponseRedirect(reverse_lazy("main:lista-avaliacao"))
+
+
+class MidiaBuscaView(View):
+    """View para buscar mídias na API OMDB"""
+    def get(self, request):
+        termo = request.GET.get('q', '')
+        resultados = []
+        
+        if termo:
+            resultados = OMDBService.buscar_multiplos(termo)
+        
+        contexto = {
+            'termo': termo,
+            'resultados': resultados,
+        }
+        return render(request, 'main/buscaMidia.html', contexto)
+
+class MidiaImportarView(View):
+    """View para importar uma mídia da OMDB para o banco"""
+    def post(self, request):
+        imdb_id = request.POST.get('imdb_id')
+        
+        if not imdb_id:
+            messages.error(request, 'ID do IMDB não fornecido')
+            return HttpResponseRedirect(reverse_lazy('main:busca-midia'))
+        
+        # Verifica se já existe
+        if Midia.objects.filter(imdb_id=imdb_id).exists():
+            messages.warning(request, 'Esta mídia já está cadastrada')
+            return HttpResponseRedirect(reverse_lazy('main:lista-midia'))
+        
+        # Busca dados da API
+        dados = OMDBService.buscar_por_imdb_id(imdb_id)
+        
+        if dados:
+            # Cria a mídia
+            midia = Midia.objects.create(
+                titulo=dados['titulo'],
+                tipo=dados['tipo'],
+                sinopse=dados['sinopse'],
+                ano_lancamento=dados['ano_lancamento'],
+                diretor=dados['diretor'],
+                generos=dados['generos'],
+                imdb_id=dados['imdb_id'],
+                poster_url=dados['poster_url'],
+            )
+            messages.success(request, f'Mídia "{midia.titulo}" importada com sucesso!')
+        else:
+            messages.error(request, 'Erro ao buscar dados da mídia')
+        
+        return HttpResponseRedirect(reverse_lazy('main:lista-midia'))
