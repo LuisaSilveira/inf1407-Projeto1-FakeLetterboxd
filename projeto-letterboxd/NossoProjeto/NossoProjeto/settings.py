e4d59abe95e0377e48142fad9d3cf4cb692577ed
rafaribeiro2013 <rafaribeiro2013@gmail.com>
2026-04-21 13:32:03 -0300

    caminho correto para as fotos de perfil

--- projeto-letterboxd/NossoProjeto/NossoProjeto/settings.py
+++ projeto-letterboxd/NossoProjeto/NossoProjeto/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGOUT_URL = 'home'
--- projeto-letterboxd/NossoProjeto/accounts/models.py
+++ projeto-letterboxd/NossoProjeto/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     data_nascimento = models.DateField(null=True, blank=True)
     bio = models.TextField(blank=True)
-    foto_perfil = models.ImageField(upload_to='perfil_fotos/', blank=True, null=True)
+    foto_perfil = models.ImageField(upload_to='accounts/static/accounts/img/perfil-fotos/', blank=True, null=True)
 
     def __str__(self):
         return self.username
