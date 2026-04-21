cb0560e72271e94b4fc14525334097ee58fa94ba
rafaribeiro2013 <rafaribeiro2013@gmail.com>
2026-04-21 13:19:59 -0300

    foto de perfil funcional

--- projeto-letterboxd/NossoProjeto/accounts/models.py
+++ projeto-letterboxd/NossoProjeto/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     data_nascimento = models.DateField(null=True, blank=True)
     bio = models.TextField(blank=True)
-    foto_perfil = models.ImageField(upload_to='static/accounts/img/perfil_fotos/', default='static/accounts/img/padrao.png') #quero puxar a foto padrao.png do static/fotos 
+    foto_perfil = models.ImageField(upload_to='perfil_fotos/', blank=True, null=True)
 
     def __str__(self):
         return self.username
